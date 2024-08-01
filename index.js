29fdba277a9040c0d3c0e7f16b28813489cbacbe
Jorge V. Contreras <contreras.jorgev@gmail.com>
2024-08-01 16:01:48 -0500

    Compute only when input is complete (tab -out of field)

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let myform = document.forms[0];
 
 if (myform.elements.username) {
-    myform.elements.username.addEventListener("input", handleNameInputChange);
+    myform.elements.username.addEventListener("change", handleNameInputChange);
 }
 if (myform.elements.birthday) {
-    myform.elements.birthday.addEventListener("input", handleDateInputChange);
+    myform.elements.birthday.addEventListener("change", handleDateInputChange);
 }
 let ageEl = myform.elements.age;
 if (ageEl.parentElement) {
